222e83ab80e22200993da393425a263e5b241df9
takuya <takuya025@gmail.com>
2014-11-26 17:49:11 +0900

    miss

---
+++ ver0029nxlarge/py_subplot2.py
@@ -0,0 +1,86 @@
+# coding:utf-8
+
+from pylab import *
+import numpy as np
+import scipy as sp
+import matplotlib.pyplot as plt
+
+
+f = figure()
+subplots_adjust(hspace=0.001)
+
+ax1 = subplot(411)
+data1 = np.loadtxt('./ex1010.d')
+x1 = data1[:,0]
+y1 = data1[:,1]
+plt.plot(x1,y1)
+
+ax2 = subplot(412)
+data2 = np.loadtxt('./ex1020.d')
+x2 = data2[:,0]
+y2 = data2[:,1]
+plt.plot(x2,y2)
+
+ax3 = subplot(413)
+data3 = np.loadtxt('./ex1030.d')
+x3 = data3[:,0]
+y3 = data3[:,1]
+plt.plot(x3,y3)
+
+# ax4 = subplot(414)
+# data4 = np.loadtxt('./ex1040.d')
+# x4 = data4[:,0]
+# y4 = data4[:,1]
+# plt.plot(x4,y4)
+
+# ax5 = subplot(615)
+# data5 = np.loadtxt('./hz1040.d')
+# x5 = data5[:,0]
+# y5 = data5[:,1]
+# plt.plot(x5,y5)
+
+# ax6 = subplot(616)
+# data6 = np.loadtxt('./hz1050.d')
+# x6 = data6[:,0]
+# y6 = data6[:,1]
+# plt.plot(x6,y6)
+
+show()
+
+
+
+# from pylab import *
+
+# t = arange(0.0, 2.0, 0.01)
+
+# s1 = sin(2*pi*t)
+# s2 = exp(-t)
+# s3 = s1*s2
+
+# # axes rect in relative 0,1 coords left, bottom, width, height.  Turn
+# # off xtick labels on all but the lower plot
+
+
+# f = figure()
+# subplots_adjust(hspace=0.001)
+
+
+# ax1 = subplot(311)
+# ax1.plot(t,s1)
+# yticks(arange(-0.9, 1.0, 0.4))
+# ylim(-1,1)
+
+# ax2 = subplot(312, sharex=ax1)
+# ax2.plot(t,s2)
+# yticks(arange(0.1, 1.0,  0.2))
+# ylim(0,1)
+
+# ax3 = subplot(313, sharex=ax1)
+# ax3.plot(t,s3)
+# yticks(arange(-0.9, 1.0, 0.4))
+# ylim(-1,1)
+
+# xticklabels = ax1.get_xticklabels()+ax2.get_xticklabels()
+# setp(xticklabels, visible=False)
+
+# show()
---
+++ ver0029nxlarge_pinhole8/py_diffmaxminplot.py
@@ -0,0 +1,40 @@
+# coding:utf-8
+from matplotlib.pyplot import *
+from pylab import *
+import numpy as np
+import matplotlib.pyplot as plt
+# subplots_adjust(hspace=0.001)
+
+fig=figure()
+
+ax1 = fig.add_subplot(211)
+ax2 = fig.add_subplot(212)
+
+
+data1 = np.loadtxt('./diff_max.d')
+posi1 = data1[:,0]
+time1 = data1[:,1]
+max1  = data1[:,2]
+
+ax1.set_title('extract max at each place')
+# ax1.set_xlabel('shot position (left)')
+ax1.set_xticks([])
+ax1.set_ylabel('differential max')
+ax1.plot(posi1,max1,label='max')
+ax1.legend()
+
+
+data2 = np.loadtxt('./diff_min.d')
+posi2 = data2[:,0]
+time2 = data2[:,1]
+min2  = data2[:,2]
+
+ax2.set_title('extract min at each place')
+ax2.set_xlabel('shot position x (2)')
+ax2.set_ylabel('differential min')
+ax2.plot(posi2,min2,label='min')
+ax2.legend()
+
+# fig.tight_layout()
+savefig('diffmaxminplot.png')
+# show() # after savefig
