94a59caf14c4a964d2807257a4363bf355ad4558
takuya <takuya025@gmail.com>
2014-12-17 20:01:01 +0900

    ver0040 Lissajous 新機軸

---
+++ ver0040/sh_python.sh
@@ -0,0 +1,26 @@
+python py_polar.py
+python py_polar2.py
+python py_polar3.py
+python py_polar4.py
+python py_polar_every.py
+python py_polar_rotation.py
+python py_polar_subplot.py
+
+python py_timediffseries23.py #time series of diff hz at each position 23
+python py_timediffseries12.py #time series of diff hz at each position 12
+
+python py_diff_max.py>diff_max.d #extract max diff hz at each poisition
+python py_diff_min.py>diff_min.d #extract min diff hz at each poisition
+
+python py_diffmaxminplot.py #extract max min at each position (differential)
+
+python py_abso_max.py>abso_max.d #extract max abso hz at each position
+python py_abso_min.py>abso_min.d #extract min abso hz at each position
+
+python py_absomaxminplot.py #extract max min at each position (absolute)
+
+python py_model.py #sig,myu model
+
+python py_diff.py # plot timeseries diff hz with and without crack at one 23 receiver position
+python py_diff12.py # " at 12 receiver position
+
