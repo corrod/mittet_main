341f5e244b652e774bd4500d52370cb5b63f6244
takuya <takuya025@gmail.com>
2014-12-19 00:16:04 +0900

    vi

--- ver0030/sh_lissajous.sh
+++ ver0030/sh_lissajous.sh
@@ -1,4 +1,4 @@
 python py_lissajous_absolute.py
 python py_lissajous_differential.py
 convert ./out_lissajous/*lissajous12.png ./out_lissajous/lissajous12.gif
-convert ./out_lissajous/*lissajous23.png ./out_lissajous/lslissajous23.gif
+convert ./out_lissajous/*lissajous23.png ./out_lissajous/lissajous23.gif
