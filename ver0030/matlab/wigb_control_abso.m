ea0cf1b403e0dc72ec2ebbf756ff2e5d6f7af24e
takuya <takuya025@gmail.com>
2014-12-23 20:06:04 +0900

    matlab wiggle表示

--- ver0030/matlab/wigb_control_abso.m
+++ ver0030/matlab/wigb_control_abso.m
@@ -1,8 +1,7 @@
 cd ./
 
-cordinate = dlmread('./out_residual/pycordinate.dat');
+cordinate = dlmread('./pycordinate.dat');
 
-%data = csvread('./out_residual/test.dat'); 
 
 data = dlmread('./combine_residual_abso.dat');
 
@@ -19,4 +18,4 @@ z = 0:1.25846858562e-6:0.00547559681601
 wigb(data(:,:),1,cordinate1,z); axis([0 85 0 0.00547559681601]);
 xlabel('Distance [mm]');
 ylabel('Travel Time [s]');
-%Title('residual_differentialmat');
+%Title('residual_absolute');
---
+++ ver0030/matlab/wigb_control_diff.m
@@ -0,0 +1,21 @@
+cd ./
+
+cordinate = dlmread('./pycordinate.dat');
+
+
+data = dlmread('./combine_residual_dii.dat');
+
+cordinate1 = transpose(cordinate);
+
+data(:,85) = [];
+
+for i = 1:4351
+    data(i, :) = data(i, :) * i^1;
+end
+
+%z = 0:1.25846858562e-6:0.00547559681601;
+z = 0:1.25846858562e-6:0.00547559681601
+wigb(data(:,:),1,cordinate1,z); axis([0 85 0 0.00547559681601]);
+xlabel('Distance [mm]');
+ylabel('Travel Time [s]');
+%Title('residual_differential');
