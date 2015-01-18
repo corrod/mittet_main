987726b3ed4b61d46605dcf5617b779993529111
takuya <takuya025@gmail.com>
2015-01-18 17:31:08 +0900

    ver0030wide アナのサイズ大きく

---
+++ chimay/diff_posi_change/ver0030wider_d/py_migration.py
@@ -0,0 +1,33 @@
+# coding:utf-8
+import numpy as np
+import scipy.signal
+from pylab import *
+from math import *
+from cmath import *
+
+nstep = 4351
+# nstep = 4351
+
+data_stac = np.loadtxt('./out_bp/20_2nd.dat')
+cases = range(20 + 1, 112 + 1)
+for i, case in enumerate(cases):
+	data = np.loadtxt('./out_bp/%s_2nd.dat' % str(case))
+	data_stac = data_stac + data
+
+np.savetxt('migration.dat',data_stac)
+
+fig = figure(figsize=(10,5))
+
+c = data_stac
+# c = np.transpose(data_stac)
+a = len(c)
+b = len(np.transpose(c))
+xlabel('sp')
+ylabel('time')
+x = arange(19, 113 + 1)
+y = arange(1, a + 1)
+X,Y = meshgrid(x, y)
+contourf(X, Y, c)
+colorbar()
+savefig('%smigration_space' % str(nstep))
+show()
